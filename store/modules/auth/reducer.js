365966b57d1f31169a1ee58f3abddf12c221a27f
Rodrigo <ro.gemaque16@gmail.com>
2021-07-29 16:41:34 -0300

    folder store for redux

---
+++ store/modules/auth/reducer.js
@@ -0,0 +1,18 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState: { loggedUser: null  } ,
+    reducers: {
+        setLoggedUser(state, action) {
+            state.loggedUser = action.payload; 
+        },
+        clearLoggedUser(state) {
+            state.loggedUser = null;
+        },
+    }
+})
+
+export const { setLoggedUser, clearLoggedUser } = authSlice.actions;
+export default authSlice.reducer;
